11ef5836e4327335c7a294502a9116483a744bcc
vij-ai <vijith.pad@gmail.com>
2020-06-29 14:41:09 +0530

    svg on login

--- screens/Loginscreen.js
+++ screens/Loginscreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { TextInput, Button, Title } from "react-native-paper";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, Dimensions } from "react-native";
 import Forminput from "../components/Forminput";
 import { useState } from "react";
 import Formbutton from "../components/Formbutton";
 import * as firebase from "firebase";
+import WavyHeader from "../components/WavyHeader";
 
 // const FIREBASE_REF_USERS = firebaseService.database().ref("Users");
 
@@ -24,43 +25,65 @@ export default function Loginscreen({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <Title style={styles.titleText}>Buda </Title>
-      <Title style={styles.titleText}> The anonymous chat app</Title>
-      <Forminput
-        labelname="Email"
-        value={Email}
-        autoCapitalize="none"
-        onChangeText={(useremail) => setEmail(useremail)}
-      />
-      <Forminput
-        labelname="Password"
-        value={Password}
-        secureTextEntry={true}
-        onChangeText={(userPassword) => setPassword(userPassword)}
-      />
-      <Formbutton
-        title="Log in"
-        modevalue="contained"
-        labelStyle={styles.loginButtonLabel}
-        onPress={() => loginUser(Email, Password)}
-      />
-      <Formbutton
-        title="Sign up"
-        modevalue="text"
-        uppercase={false}
-        labelStyle={styles.navButtonText}
-        onPress={() => navigation.navigate("Sign up")}
-      />
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        // justifyContent: "space-between",
+      }}
+    >
+      <View style={{ flex: 1, backgroundColor: "#f5f5f5" }}>
+        <WavyHeader
+          customStyles={styles.svgCurve}
+          customHeight={160}
+          customTop={130}
+          customBgColor="#5000ca"
+          customWavePattern="M0,96L48,112C96,128,192,160,288,186.7C384,213,480,235,576,213.3C672,192,768,128,864,128C960,128,1056,192,1152,208C1248,224,1344,192,1392,176L1440,160L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+        />
+      </View>
+      <View style={styles.container}>
+        <Title style={styles.titleText}>Buda </Title>
+        <Title style={styles.titleText}> The anonymous chat app</Title>
+        <Forminput
+          labelname="Email"
+          value={Email}
+          autoCapitalize="none"
+          onChangeText={(useremail) => setEmail(useremail)}
+        />
+        <Forminput
+          labelname="Password"
+          value={Password}
+          secureTextEntry={true}
+          onChangeText={(userPassword) => setPassword(userPassword)}
+        />
+        <Formbutton
+          title="Log in"
+          modevalue="contained"
+          labelStyle={styles.loginButtonLabel}
+          onPress={() => loginUser(Email, Password)}
+        />
+        <Formbutton
+          title="Sign up"
+          modevalue="text"
+          uppercase={false}
+          labelStyle={styles.navButtonText}
+          onPress={() => navigation.navigate("Sign up")}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  svgCurve: {
+    position: "absolute",
+    width: Dimensions.get("window").width,
+  },
   container: {
+    flex: 2,
     backgroundColor: "#f5f5f5",
-    flex: 1,
-    justifyContent: "center",
+
+    //justifyContent: "center",
     alignItems: "center",
   },
   titleText: {
